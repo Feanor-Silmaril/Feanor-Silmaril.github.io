8701e3de758486cd7ce69c097d6b47e59855fdb4
Feanor-Silmaril <ceche035@fiu.edu>
2015-11-07 10:22:31 -0500

    t1

--- js/grayscale.js
+++ js/grayscale.js
@@ -18,7 +18,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top -50
+            scrollTop: $($anchor.attr('href')).offset().top +250
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
